ab1497ccc272cb949fc30212961b46235539a78c
agrach060 <agrach@uw.edu>
2023-06-23 18:10:24 -0700

    Completed HW1 assignment

---
+++ HW1/README.md
@@ -0,0 +1,17 @@
+# HW1 
+## Shuffled Digits
+
+This program takes a 1 command line argument, which represents the total number of shuffles it will print out, assuming it is <= 10.
+Then the program takes the string of 9 digits through console input. It prompts the user to type in the string of 9 digits without spaces or any other characters between each digit. Otherwise it prints an error message.
+The program then randomly shuffles the 9 digits and prints them in 3x3 format.
+
+Below is the output of this program showing the command line argument as 2, and the 9 digits user input as “123456789”, and the corresponding output:
+
+![Screenshot showing the command line argument as 2, and the 9 digits user input as “123456789”, and the corresponding output](cm2in123456789.png)
+Another example of the output is when the user inputs incorrect number of digits:
+
+![Screenshot showing the output when the user inputs incorrect number of digits](incorrect_input.png)
+
+Finally, the screenshot showing the output when the number of shuffles is 6 and the user input is correct. The number of shuffle is spelled out instead of a number:
+
+![Screenshot showing the command line argument as 2, and the 9 digits user input with number of shuffles being spelled out](cd6spelled_out.png)
--- HW1/shuffledDigits.cpp
+++ HW1/shuffledDigits.cpp
@@ -2,9 +2,12 @@
 #include <string>
 #include <random>
 #include <algorithm>
+#include <cctype>
+#include <cstdlib>
 
 using namespace std;
 
+// takes a string of digits and shuffles them randomly 
 void shuffleDigits(string digits){
     random_device rd;
     mt19937 rng(rd());
@@ -20,13 +23,30 @@ void shuffleDigits(string digits){
     cout << "+-------+" << endl;
 }
 
-int main(){
+// checks if a given string contains any non-digit characters
+bool containsNonDigits(const std::string& str) {
+    for (char c : str) {
+        if (!std::isdigit(c)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// handles the user input and shuffling process
+int main(int argc, char* argv[]){
+    // prompt the user to input 9 digits
     string userInput;
-    int shuffles;
     cout << "Please input 9 digits to be randomly shuffled: " << endl;
     cin >> userInput;
-    cout << "Please enter the number of shuffles: " << endl;
-    cin >> shuffles;
+    // check if the input is valid
+    if (userInput.length() != 9 || containsNonDigits(userInput)) {
+        cout << "Invalid input. Please enter a string of 9 digits only." << endl;
+        return 1;
+    }
+    // retrieve the number of shuffles (assuming it's <= 10) from the command line argument 
+    int shuffles = atoi(argv[1]);
+    // iterate through the shuffles and print out the grid
     for (int i = 1; i <= shuffles; i++) {
         string shuffleLabel;
         if (i == 1) {
@@ -35,8 +55,20 @@ int main(){
             shuffleLabel = "second";
         } else if (i == 3) {
             shuffleLabel = "third";
-        } else {
-            shuffleLabel = to_string(i) + "th";
+        } else if (i == 4) {
+            shuffleLabel = "fourth";
+        } else if (i == 5) {
+            shuffleLabel = "fifth";
+        } else if (i == 6) {
+            shuffleLabel = "sixth";
+        } else if (i == 7) {
+            shuffleLabel = "seventh";
+        } else if (i == 8) {
+            shuffleLabel = "eighth";
+        } else if (i == 9) {
+            shuffleLabel = "ninth";
+        } else if (i == 10) {
+            shuffleLabel = "tenth";
         }
         cout << "The " << shuffleLabel << " shuffled output is: " << endl;
         shuffleDigits(userInput);
