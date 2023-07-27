d6aa68f365f079ada589e8e498c7c08a2d1de513
agrach060 <agrach@uw.edu>
2023-07-26 18:04:27 -0700

    Completed 3c

--- HW3/3b.cpp
+++ HW3/3b.cpp
@@ -1,4 +1,4 @@
-// Solved using the class notes and 
+// Solved using the class notes and
 // this video for concept idea of the dfs: https://www.youtube.com/watch?v=f2EfGComRKM
 
 /*
@@ -22,27 +22,33 @@ public:
 };
 */
 
-class Solution {
+class Solution
+{
 public:
-    Node* cloneGraph(Node* node) {
+    Node *cloneGraph(Node *node)
+    {
         // check if the node exists
-        if(node == nullptr)
+        if (node == nullptr)
             return nullptr;
-        vector<Node*> visited(101, nullptr);
+        vector<Node *> visited(101, nullptr);
         // create a new node for the clone graph
-        Node* copy = new Node(node->val);
+        Node *copy = new Node(node->val);
         // mark the node as visited
         visited[node->val] = copy;
         // visit all the neightbours of the node
-        for(Node* curr : node->neighbors){
-            if(visited[curr->val] == nullptr){
+        for (Node *curr : node->neighbors)
+        {
+            if (visited[curr->val] == nullptr)
+            {
                 // if the node hasn't been visited, create this node for the clone graph
-                Node* newNode = new Node(curr->val);
+                Node *newNode = new Node(curr->val);
                 // make it a copy node's neighbor
                 copy->neighbors.push_back(newNode);
                 // perform the dfs on this node
                 dfs(curr, newNode, visited);
-            }else{
+            }
+            else
+            {
                 // if the node has been visited, add the node from the visited array as its neighbor
                 copy->neighbors.push_back(visited[curr->val]);
             }
@@ -50,22 +56,27 @@ public:
         return copy;
     }
 
-    void dfs(Node* curr, Node* node, vector<Node*>& visited){
+    void dfs(Node *curr, Node *node, vector<Node *> &visited)
+    {
         // curr - current original node
         // node - current clone node
-        // mark the newly made node as visited 
+        // mark the newly made node as visited
         visited[node->val] = node;
         // go through all the adjacent nodes of the original node
-        for(Node* adj : curr->neighbors){
+        for (Node *adj : curr->neighbors)
+        {
             // if the neighbor is not visited
-            if(visited[adj->val] == nullptr){
+            if (visited[adj->val] == nullptr)
+            {
                 // create a new node
-                Node* newNode = new Node(adj->val);
+                Node *newNode = new Node(adj->val);
                 // make it a current node's neighbor
                 node->neighbors.push_back(newNode);
                 dfs(adj, newNode, visited);
-            }else{
-                // if the node has been visited, take this node from the visited array  
+            }
+            else
+            {
+                // if the node has been visited, take this node from the visited array
                 // and make it a current node's neighbor
                 node->neighbors.push_back(visited[adj->val]);
             }
--- HW3/3c.cpp
+++ HW3/3c.cpp
@@ -1,1 +1,57 @@
+// Solved using the class notes and
+// the help from this video: https://www.youtube.com/watch?v=uspsIxavTzA
 
+class Solution
+{
+public:
+    int numIslands(vector<vector<char>> &grid)
+    {
+        // define the number of vectors in the outer vector
+        int rows = grid.size();
+        // if the grid is empty, return 0 islands
+        if (rows == 0)
+            return 0;
+        // define the number of vectors in the inner vector
+        int cols = grid[0].size();
+        // if the grid is empty, return 0 islands
+        if (cols == 0)
+            return 0;
+
+        int count = 0;
+        // check how many islands are in the grid by exploring the neighbors of 1s
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    void dfs(vector<vector<char>> &grid, int x, int y)
+    {
+        grid[x][y] = '0';
+        // look for the neighbors in 4 directions
+        if (x + 1 < grid.size() && grid[x + 1][y] == '1')
+        {
+            dfs(grid, x + 1, y);
+        }
+        if (x - 1 >= 0 && grid[x - 1][y] == '1')
+        {
+            dfs(grid, x - 1, y);
+        }
+        if (y - 1 >= 0 && grid[x][y - 1] == '1')
+        {
+            dfs(grid, x, y - 1);
+        }
+        if (y + 1 < grid[0].size() && grid[x][y + 1] == '1')
+        {
+            dfs(grid, x, y + 1);
+        }
+    }
+};
